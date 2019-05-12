85bbf7dff2776262e377524facb4b9634057c432
GeMarK[可爱飞行猪] <golang83@outlook.com>
2019-05-12 14:09:56 +0800

    fix setup path.

--- ScheduleTask.bat
+++ ScheduleTask.bat
@@ -1,3 +1,3 @@
 @echo off
-set BW="C:\Program Files (x86)\微软必应每日壁纸\BingWallpapers.exe"
-schtasks /create /tn "必应壁纸自动获取A" /tr %BW% /sc daily /st 14:00:00 /ed 2025/05/02
+set BW="D:\BingWallpapers\BingWallpapers.exe"
+schtasks /create /tn "蹇呭簲澹佺焊鑷姩鑾峰彇A" /tr %BW% /sc daily /st 14:00:00 /ed 2025/05/02
